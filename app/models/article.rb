574c74000d78a82180ed279828a2a56a52a31e43
aalbudoor <aalfalasi1@sheffield.ac.uk>
2024-01-17 17:52:57 +0400

    Adding validations in article model and allowing to display error messages in the new article view form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
 class Article < ApplicationRecord
+    # Rails provides a feature called validations to help us deal with invalid user input. 
+    # alidations are rules that are checked before a model object is saved. If any of the checks fail, the save will be aborted, 
+    # and appropriate error messages will be added to the errors attribute of the model object.
+
+    # The first validation declares that a title value must be present. Because title is a string, 
+    # this means that the title value must contain at least one non-whitespace character.
+    validates :title, presence: true
+
+    # The second validation declares that a body value must also be present. 
+    # Additionally, it declares that the body value must be at least 10 characters long.
+    validates :body, presence: true, length: {minimum: 10}
 end
