384fe3a2767a7d45a8c0afbaa2881cb512788395
aalbudoor <aalfalasi1@sheffield.ac.uk>
2024-01-18 21:17:30 +0400

    Initiated Edit and Update methods in article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,26 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # The edit action fetches the article from the database, and stores it in @article so that it can be used 
+  # when building the form. By default, the edit action will render app/views/articles/edit.html.erb.
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # The update action (re-)fetches the article from the database, and attempts to update it with the submitted form data 
+  # filtered by article_params. If no validations fail and the update is successful, the action redirects the browser to the article's 
+  # page. Else, the action redisplays the form — with error messages — 
+  # by rendering app/views/articles/edit.html.erb.
+  def update
+    @article = Article.find(params[:id])
+
+    if article.save
+      redirect_to @article
+    else
+      rends :new, status: :unprocessable_entity
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
